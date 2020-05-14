5561ba373c3fe5706706266567efd043bd59e599
Carlos Vaquedano <chvb2002@hotmail.com>
2020-05-13 23:00:54 -0600

    agregar metodo que retorne usuario autenticado

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const {check} = require('express-validator');
-
+const auth = require('../middleware/auth');
 // Crea un usuario
 // api/auth
 router.post('/',
@@ -11,5 +11,11 @@ router.post('/',
         check('password','El password debe ser minimo de 6 caracteres').isLength({min:6})
     ],
     authController.autenticarUsuario
+);
+
+// Obtiene el usuario auntenticado
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
 );
 module.exports = router;
