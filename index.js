d6fee9ef0871622135b0d438657a1d2f9893c9c0
Carlos Vaquedano <chvb2002@hotmail.com>
2020-05-15 21:20:28 -0600

    rollback

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -28,6 +28,6 @@ app.get('/',(req,res)=>{
     res.send('Hola mundo');
 });
 
-app.listen(port,'0.0.0.0', ()=>{
+app.listen(PORT,()=>{
     console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 });
